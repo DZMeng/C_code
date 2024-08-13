241e7f93ef2c0f826636996ea040f7c112a787d1
DZMeng <mdz2534@163.com>
2024-08-13 14:45:26 +0800

    测试GitHub

---
+++ P12-P15/P12-P15/P12-P15.cpp
@@ -0,0 +1,88 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+//选择语句
+
+//int main()
+//{
+//	int input = 0;
+//	printf("加入比特\n");
+//	printf("要好好学习吗(1/0)？");//选项
+//	scanf("%d", &input);//输入选项
+//	if (input == 1)//选择语句，选择选项
+//	{
+//		printf("好offer\n");//选项的结果
+//	}
+//	else//另一选项
+//	{
+//		printf("卖红薯\n");//另一选项的结果
+//	}
+//	return 0;
+//}
+
+//循环语句
+//C语言中，循环语句分为：while循环，for循环，do...while循环。
+
+//20000行有效代码为终止条件
+//int main()
+//{
+//	int line = 0;
+//	printf("加入比特\n");
+//	while (line < 20000)
+//	{
+//		printf("写代码%d\n",line);
+//		line++;
+//	}
+//	if (line >= 20000)
+//	{
+//		printf("好offer\n");
+//	}
+//	else
+//	{
+//		printf("继续加油\n");
+//	}
+//	
+//	return 0;
+//}
+
+//函数
+
+//int Add(int x, int y)//写函数
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int n1 = 0;
+//	int n2 = 0;
+//	scanf("%d %d", &n1, &n2);
+	//int sum = n1 + n2;
+//	int sum = Add(n1, n2);
+//	printf("%d", sum);
+//	return 0;
+//}
+
+//数组:一组相同类型元素的集合
+
+//int main()
+//{
+	//int a = 10;
+	//int b = 11;
+	//int c = 12;
+	//0~9
+	//int arr[10] = { 0,1,2,3,4,5,6,7,8,9 };//下标从0开始
+	//char ch[5];
+	//double d[30];
+	//printf("%d\n",arr[6]);
+	//int i = 0;
+	//while (i < 10)
+	//{
+		//printf("%d ", arr[i]);
+		//i=i+1;
+	//}
+	//return 0;
+//}
+
+//
